158f3e0e0b3fac18791be13f7db63e1e2bc673d2
alexander gubernikoff <agubernikoff@gmail.com>
2022-02-07 17:15:28 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .querySelector("#create-task-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+      createDescription(e.target["new-task-description"].value);
+      //function createDescription()
+    });
 });
+function createDescription(newTask) {
+  const tasks = document.querySelector("#tasks");
+  const p = document.createElement("p");
+  const btn = document.createElement("button");
+  btn.innerText = "x";
+  p.innerText = `${newTask}  `;
+  p.appendChild(btn);
+  tasks.append(p);
+  document.querySelector("#create-task-form").reset();
+  btn.addEventListener("click", function (e) {
+    e.target.parentNode.remove();
+  });
+}
